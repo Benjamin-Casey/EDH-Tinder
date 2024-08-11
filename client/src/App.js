9097bb82a1d0d3e98d25768ced66ecbaab0a5d17
Benjamin Casey <benjamincasey@Benjamins-Laptop.local>
2024-08-11 15:43:00 +1000

    Got server + client running. Started working on front-end with Bootstrap.

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,40 @@
-import axios from 'axios';
-import './App.css';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
 
-const apiCall = () => {
-  axios.get('http://localhost:8080').then((data) => {
-    console.log(data);
-  });
-}
+import './Custom.css';
 
 function App() {
   return (
-    // TODO: replace this with some index.html - google naming convention
-    <div className="App">
-      <header className="App-header">
-        <button onClick={apiCall}>Make API Call</button>
-      </header>
-    </div>
+    <Container className="p-3">
+      <h1 className="header">EDH Tinder!</h1>
+
+      {/* Create a tab for commanders that have been liked */}
+
+
+
+      {/* Create a container for the commander image */}
+
+      <Container className="p-5">
+        <h2>Commander:</h2>
+
+        {/* TODO: remove/replace */}
+        <div class="card">
+          <img src="https://placehold.co/200x400" class="card-img-top"/>
+
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+
+      </Container>
+
+      {/* Create buttons */}
+      <Button className="rounded-circle p-4">Like</Button>
+
+
+    </Container>
   );
 }
+export default App;
