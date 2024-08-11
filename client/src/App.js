81fb0a51128345b54dc724105d8616eeaa111f67
Benjamin Casey <benjamincasey55@gmail.com>
2024-08-11 19:09:21 +1000

    Fixed card and added second tab for saved commanders

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,40 @@
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
 
-import './Custom.css';
 
 function App() {
   return (
     <Container className="p-3">
-      <h1 className="header">EDH Tinder!</h1>
+      <h1 className="header">Howdy!</h1>
 
       {/* Create a tab for commanders that have been liked */}
+      <Tabs className="mb-3">
+        <Tab eventKey="random-commander" title="Random Commander">
+          {/* TODO: import component, - move code for commander etc into a new file*/}
+            <h2>Pard:</h2>
+            <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top" src="holder.js/100px180" />
+            </Card>
+        </Tab>
+        <Tab eventKey="saved-commanders" title="Saved Commanders">
+          <h2>Here's tab 2</h2>
+        </Tab>
+      </Tabs >
+    {/* </Container> */}
+
+
+  {/* Create a container for the commander image */ }
 
 
 
-      {/* Create a container for the commander image */}
-
-      <Container className="p-5">
-        <h2>Commander:</h2>
-
-        {/* TODO: remove/replace */}
-        <div class="card">
-          <img src="https://placehold.co/200x400" class="card-img-top"/>
-
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-          </div>
-        </div>
-
-      </Container>
-
-      {/* Create buttons */}
-      <Button className="rounded-circle p-4">Like</Button>
+  {/* Create buttons */ }
+  <Button className="rounded-circle p-4">Like</Button>
 
 
-    </Container>
+    </Container >
   );
 }
 export default App;
