367a5a99259d31ab985f417fa77b43676e4b40e9
Benjamin Casey <benjamincasey@Benjamins-Laptop.local>
2024-08-17 16:05:40 +1000

    Got commander images and fixed buttons. Note: function to get images is commented out on CommanderPane:24 to prevent API calls.

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
+import CommanderPane from './components/CommanderPane';
+
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Card from 'react-bootstrap/Card';
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
-
 
 function App() {
   return (
@@ -12,26 +13,24 @@ function App() {
 
       {/* Create a tab for commanders that have been liked */}
       <Tabs className="mb-3">
-        <Tab eventKey="random-commander" title="Random Commander">
+        <Tab eventKey="random-commander" title="Random Commander" align="center">
           {/* TODO: import component, - move code for commander etc into a new file*/}
-            <h2>Pard:</h2>
-            <Card style={{ width: '18rem' }}>
-              <Card.Img variant="top" src="holder.js/100px180" />
-            </Card>
+          <CommanderPane />
+          {/* <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="holder.js/100px180" />
+          </Card> */}
         </Tab>
         <Tab eventKey="saved-commanders" title="Saved Commanders">
           <h2>Here's tab 2</h2>
         </Tab>
       </Tabs >
-    {/* </Container> */}
 
-
-  {/* Create a container for the commander image */ }
+      {/* Create a container for the commander image */}
 
 
 
-  {/* Create buttons */ }
-  <Button className="rounded-circle p-4">Like</Button>
+      {/* Create buttons */}
+      {/* <Button className="rounded-circle p-4">Like</Button> */}
 
 
     </Container >
--- client/src/components/CommanderPane.js
+++ client/src/components/CommanderPane.js
@@ -1,21 +1,47 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Container from "react-bootstrap/esm/Container";
+import Button from "react-bootstrap/esm/Button";
 
-function GetImage() {
-  // TODO: make a call to Scryfall API to get commander image + data
+function addToLikedCommanders() {
+  // TODO
 }
 
 function CommanderPane() {
+  const [commanderData, setCommanderData] = useState(null); // initialize with null
+
+  // On page load
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://api.scryfall.com/cards/random?q=is%3Acommander');
+        setCommanderData(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    // fetchData();
+
+    console.log("Got commander data:");
+    console.log(commanderData);
+  }, []);
+
   return (
     <Container>
-      <Container className="image-container">
-        {/* Use scryfall API to get an image here under a function above. */}
-
+      <Container className="image-container p-3">
+        {commanderData && ( // Check if data exists before rendering
+          <img src={commanderData.image_uris.normal} alt={commanderData.name} /> // Access image URL from response
+        )}
       </Container>
-      <Container className="button-container">
-        {/* Put buttons here to "swipe left or right" on commanders */}
-        
-      </Container>
+      {/* 
+          On dislike, just get new commander.
+          On like, add to database to display on the new tab page.
+      */}
+      <Button onClick={getNewCommander} variant="danger">Dislike</Button>{' '}
+      <Button onClick={addToLikedCommanders} variant="success">Like</Button>{' '}
     </Container>
   );
 }
+
 export default CommanderPane;
